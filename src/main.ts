1ac447d55f9692100296fe2ddfb6ae95b246825b
Gustavo Ybanez <gustavoybannez@gmail.com>
2025-05-15 20:39:19 -0300

    Agregada configuración de cors para pruebas locales

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Configuración de CORS
+  app.enableCors({
+    origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Elimina propiedades no decoradas
